6198ac0c8e860aa3ea0679b6de180409951fdf23
Alexis La Porte <aklaporte@gmail.com>
2015-11-20 15:42:18 -0500

    add providers to Gemfile remove outdated columns from migrations and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+
   get 'register', :to => 'users#new', as: :register
   get 'logout', :to => 'sessions#destroy', as: :logout
 
@@ -13,5 +14,7 @@ Rails.application.routes.draw do
   get '/login', :to => 'sessions#new', as: :login
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/failure', :to => 'sessions#failure'
+
+
 
 end
