7b6bc9deef3e3775808e3c8f8b2882809555ba20
Andre Jesse <andre.isaac.jessee@gmail.com>
2015-11-20 14:34:59 -0500

    added tests to band_spec, not working yet

--- spec/models/band_spec.rb
+++ spec/models/band_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Band do
-  it "exists" do
-    expect(Band.new).to be_a(Band)
+  context 'is a model and has attributes' do
+
+    it "exists" do
+      expect(Band.new).to be_a(Band)
+    end
+
+    it 'has a name attribute' do
+     expect(Band.new).to respond_to(:name)
+   end
+
+    it 'has a bio attributes' do
+     expect(Band.new).to respond_to(:bio)
+   end
+
+    it 'has an admin id' do
+     expect(Band.new).to respond_to(:admin_id)
+   end
+
   end
 end
