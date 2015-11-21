c9ca14aa6de78bbfd904a647c727b14e909906a8
Anders Drew <anders.j.drew@gmail.com>
2015-11-21 15:33:44 -0500

    Add uid / provider columns to the users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120203140) do
+ActiveRecord::Schema.define(version: 20151121201642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,8 @@ ActiveRecord::Schema.define(version: 20151120203140) do
     t.text     "bio"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
 end
