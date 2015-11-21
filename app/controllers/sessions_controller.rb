077eda1575f4605d6aa41b0098599cc9c55a01a8
Alexis La Porte <aklaporte@gmail.com>
2015-11-21 13:19:44 -0500

    fix broken link between user and authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,28 @@ class SessionsController < ApplicationController
   def create
     auth = request.env['omniauth.auth']
     # session[:omniauth] =  auth.except('extra')
-    @authentication = Authentication.find_or_create_with_omniauth(auth)
+    @authentication = Authentication.find_with_omniauth(auth)
 
-    @authentication.user = current_user
-    @authentication.save
-    redirect_to root_path, notice: "SIGNED IN"
+    if @authentication.nil?
+     @authentication = Authentication.create_with_omniauth(auth)
+    end
+
+    if logged_in?
+      if @authentication.user == current_user
+        redirect_to root_path, notice: "SIGNED IN"
+      else
+        @authentication.user = session[:user_id]
+        @authentication.save
+        redirect_to root_path, notice: "SIGNED IN"
+      end
+     else
+       if @authentication.user.present?
+         self.current_user = @authentication.user
+        redirect_to root_path, notice: "SIGNED IN"
+       else
+        render :new
+      end
+    end
   end
 
   def destroy
