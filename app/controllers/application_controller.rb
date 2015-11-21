07314dbbe80b54d6f99c7d10429fec80b70849d9
NiqueDaSneak <niquedasneak4@gmail.com>
2015-11-20 19:40:25 -0500

    Fix current_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,9 @@ class ApplicationController < ActionController::Base
     current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def logged_in? 
-  	session[:user_id] = current_user.id
-  end 
+  def logged_in?
+    !!current_user
+  end
 
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
 end
