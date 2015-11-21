35c8951e09d4ee20b6f1859de6477016ff02b43d
NiqueDaSneak <niquedasneak4@gmail.com>
2015-11-20 21:09:28 -0500

    Create search route; insert logic for form renders based on initial search selection

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,15 @@
 class WelcomeController < ApplicationController
   def index
-    @instruments = Instruments.all
-    @genres = Genres.all
   end
+
+  def search
+    @instruments = Instrument.all
+    @genres = Genre.all
+    if params[:type] == 'musicians'
+      render :'musician_search'
+    else
+      render :'band_search'
+    end
+  end
+
 end
