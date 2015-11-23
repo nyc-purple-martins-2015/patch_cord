51face390c1c0d39ef62af0f9cc16533d0845de5
Alexis La Porte <aklaporte@gmail.com>
2015-11-23 15:41:24 -0500

    pull user create logic into model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,15 @@ class User < ActiveRecord::Base
   has_many :genres, through: :user_genres
   has_many :bands, through: :user_bands
   has_many :media_resources, as: :showable
+
+  def self.validate_via_provider(auth)
+    @user = User.find_or_create_by(provider: auth[:provider], uid: auth[:uid])
+    @user.username = auth[:info][:name]
+    if @user.new_record?
+      @user.password = SecureRandom.uuid()
+      @user.phone = '122345689'
+      @user.save!
+    end
+  end
+
 end
