686abf7447176f656ddbd0b54f581f276cddfee0
Alexis La Porte <aklaporte@gmail.com>
2015-11-23 17:33:18 -0500

    add ability to delete specific instruments from user edit profile

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,30 +1,30 @@
 class InstrumentsController < ApplicationController
-	
-	def new 
+
+	def new
 		@instrument = Instrument.new
 	end
 
 	def create
 		@user = current_user
 		@instrument = @user.instruments.new(instrument_params)
-		if @instrument.save 
-			redirect_to root_path 
-		else 
+		if @instrument.save
+			redirect_to root_path
+		else
 			render :new
-		end 
+		end
 	end
 
 	def destroy
 		@user = current_user
 		@instrument = @user.instruments.find(params[:id])
-		@instrument.destroy 
-		redirect_to root_path
+		@instrument.destroy
+		redirect_to edit_user_path(@user)
 	end
 
-	private 
+	private
 
-	def instrument_params 
+	def instrument_params
 		instrument_params = params.require(:instrument).permit(:name)
 	end
 
-end 
+end
