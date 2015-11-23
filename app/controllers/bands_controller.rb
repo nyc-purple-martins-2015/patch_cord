aadbddfdcf1dd9f75816976633d369c2e1e9b54d
maecapozzi <maecapozzi@gmail.com>
2015-11-23 09:36:04 -0500

    fix band edit

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -10,9 +10,9 @@ class BandsController < ApplicationController
 
   def new
     @band = Band.new
-	end
+  end
 
-	def create
+  def create
     @band = current_user.bands.new(band_params)
     if @band.save
       redirect_to @band
@@ -20,21 +20,20 @@ class BandsController < ApplicationController
       status 400
       render :new
     end
-	end
+  end
 
   def edit
-     @band = Band.find(params[:id])
-    @genres = Genre.pluck(:name)
-   end
+   @band = Band.find(params[:id])
+   @genres = Genre.pluck(:name)
+  end
  
-   def update
-     @band = Band.find(params[:id])
-    if @band
-      @band.update_attributes(band_params)
+  def update
+   @band = Band.find(params[:id])
+   if @band
+    @band.update_attributes(band_params)
     @members = @band.users
 
     if @band.save
-      binding.pry
       new_admin = params[:band][:admin_name]
       @band.admin = User.find_or_create_by(username: new_admin.strip)
 
@@ -42,48 +41,48 @@ class BandsController < ApplicationController
         @genres = params[:genre_types]
         @genres.each do |genre|
           unless @band.genres.map(&:name).include?(genre)
-          @band.genres << Genre.find_or_create_by(name: genre.strip)
+            @band.genres << Genre.find_or_create_by(name: genre.strip)
           end
         end
-      end
 
+      end
       new_members = params[:band][:members].split(",")
       if new_members.any?
         new_members.each do |member|
           unless @members.map(&:username).include?(member)
-          @members << User.find_or_create_by(username: member.strip)
+            @members << User.find_or_create_by(username: member.strip)
           end
         end
       end
 
-     @band.update_attributes(band_params)
-       redirect_to band_path(@band)
-     else
-       render :edit
-     end
+      @band.update_attributes(band_params)
+      redirect_to band_path(@band)
+    else
+     render :edit
+   end
  end
-  end
- 
-  def destroy
-     if @band.destroy
-       redirect_to root_path
-     else
-       @errors = @band.errors.full_messages
-     end
-  end
-  def search
-    genre_ids = params["Genre"].map {|e| e[0].to_i}
-    genres = Genre.find(genre_ids)
-    genres_with_bands = genres.select {|genre| !genre.bands.empty?}
-    bands_from_genre = genres_with_bands.map {|genre| genre.bands}.flatten
-    band_ids = bands_from_genre.map {|band| band.id}
-    @bands = Band.find(band_ids)
-    render "bands/_bandssorted", layout: false
-  end
+end
 
-	private
+def destroy
+ if @band.destroy
+   redirect_to root_path
+ else
+   @errors = @band.errors.full_messages
+ end
+end
+def search
+  genre_ids = params["Genre"].map {|e| e[0].to_i}
+  genres = Genre.find(genre_ids)
+  genres_with_bands = genres.select {|genre| !genre.bands.empty?}
+  bands_from_genre = genres_with_bands.map {|genre| genre.bands}.flatten
+  band_ids = bands_from_genre.map {|band| band.id}
+  @bands = Band.find(band_ids)
+  render "bands/_bandssorted", layout: false
+end
 
-	def band_params
-		band_params = params.require(:band).permit(:name, :bio, :admin_id)
-	end
+private
+
+def band_params
+  band_params = params.require(:band).permit(:name, :bio, :admin_id)
+end
 end
