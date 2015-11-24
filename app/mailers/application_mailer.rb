b775fe0c09da3a6807601ec8a050e54a2f8fbb22
Mae Capozzi <email": null>
2015-11-24 14:29:15 -0500

    edit application_mailer and user_mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "maecapozzi@gmail.com"
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,14 @@
 class UserMailer < ApplicationMailer
-  default from: "from@example.com"
+  default from: "maecapozzi@gmail.com"
 
-  def musician_email(user)
+  def welcome_email(user)
+    @user = user
+    mail(to: @user.email, subject: "Thanks for joining!")
+  end
+
+  def user_email(user)
     @user = user
     mail(to: @user.email, subject: 'Someone wants to jam!')
   end
+
 end
