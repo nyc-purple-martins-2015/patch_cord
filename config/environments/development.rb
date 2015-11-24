b1f72b07b2afe3ea5f4a207f0f227eb4dcc4adf5
Mae Capozzi <email": null>
2015-11-24 14:28:30 -0500

    fix config files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,11 +41,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_pw'],
-   :authentication       => "plain",
-  :enable_starttls_auto => true
+   :address              => "localhost",
+   :port                 => 1025
   }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,10 +81,11 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
-    :port => 587,,
+    :port => 587,
+    :domain => "gmail.com"
     :user_name => ENV['gmail_username'],
     :password => ENV["gmail_password"],
-    :authentication => "plain",
+    :authentication => :plain,
   :enable_starttls_auto => true
 }
 end
