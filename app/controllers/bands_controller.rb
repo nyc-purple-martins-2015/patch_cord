8d23f77c9fe8b2a53b232abe097e6d0ae5dc462d
Alexis La Porte <aklaporte@gmail.com>
2015-11-23 20:04:57 -0500

    edit routes to delete band members, fix view of index of bands

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -9,9 +9,10 @@ class BandsController < ApplicationController
   end
 
   def create
-    @band = current_user.bands.new(band_params)
+    @band = Band.new(band_params)
+    @band.admin = current_user
     if @band.save
-      redirect_to @band
+      redirect_to bands_path()
     else
       status 400
       render :new
@@ -69,16 +70,15 @@ end
  def destroy
    @band = Band.find(params[:id])
 
-   if @band
+   if @band && params[:member]
+     UserBand.find_by(user_id: params[:member]).destroy
+     redirect_to band_path(@band)
+   elsif @band
      @band.destroy
      redirect_to root_path
    else
      @errors = @band.errors.full_messages
    end
-
-   # @member = @band.users.find(params[:id])
-   # @member.destroy
-   # redirect_to edit_band_path(@band)
  end
 
  def search
