552ab4100e63739bec23992c82b0148bc2cdffc6
Mae Capozzi <email": null>
2015-11-24 12:52:35 -0500

    add config info

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,14 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => ENV['gmail_username'],
+   :password             => ENV['gmail_pw'],
+   :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
 end
