a2578cc2d674b3a3162a77a64c97a1848eece213
NiqueDaSneak <niquedasneak4@gmail.com>
2015-11-24 10:08:59 -0500

    Write jQuery to show band media resources and embed soundcloud

--- app/assets/javascripts/band_show.js
+++ app/assets/javascripts/band_show.js
@@ -8,6 +8,13 @@ $(document).ready(function(){
     })
     .done(function(response){
       $('.media-resources').html(response)
+        $('a[href*="soundcloud.com"]').each(function(){
+        var $link = $(this);
+        $.getJSON('http://soundcloud.com/oembed?format=js&url=' + $link.attr('href') + '&iframe=true&callback=?', function(response){
+         $link.replaceWith(response.html);
+    });
+  });
+
     });
   });
 });
