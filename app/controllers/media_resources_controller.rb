8097eeb21dc8f666125a27349714b3ab033e817b
Andre Jesse <andre.isaac.jessee@gmail.com>
2015-11-25 13:36:01 -0500

    got media resources working for both users and bands

--- app/controllers/media_resources_controller.rb
+++ app/controllers/media_resources_controller.rb
@@ -1,11 +1,26 @@
 class MediaResourcesController < ApplicationController
 
 	def new
+    if params[:user_id]
+      @showable_type = "User"
+      @showable_id = params[:user_id]
+    elsif params[:band_id]
+      @showable_type = "Band"
+      @showable_id = params[:band_id]
+    end
 	end
 
 	def create
-		current_user.media_resources.create(media_resources_params)
-			redirect_to user_path(current_user)
+    puts params
+    if params[:media_resources][:showable_type] == "User"
+  		mr = MediaResource.new(media_resources_params)
+  		mr.showable = current_user
+      mr.save!
+      redirect_to user_path(current_user)
+    elsif params[:media_resources][:showable_type] == "Band"
+      MediaResource.create!(media_resources_params)
+      redirect_to band_path(params[:media_resources][:showable_id])
+    end
 	end
 
 	def edit
@@ -30,6 +45,6 @@ end
 	private
 
 	def media_resources_params
-		media_resources_params = params.require(:media_resources).permit(:media_type, :content, :link)
+		media_resources_params = params.require(:media_resources).permit(:media_type, :content, :link, :showable_id, :showable_type)
 	end
 end
