b17ecc628e6ae8d3fb3485a8f5136a5238da833d
Mae Capozzi <email": null>
2015-11-25 09:54:33 -0500

    fix syntax error in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,10 +82,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :domain => "gmail.com"
+    :domain => "gmail.com",
     :user_name => ENV['gmail_username'],
     :password => ENV["gmail_password"],
-    :authentication => :plain,
-  :enable_starttls_auto => true
 }
 end
